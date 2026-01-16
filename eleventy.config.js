9b445548ed6c622d5fb52a43d39643ed16149e5f
Makenna Herl <hire.makenna.herl@gmail.com>
2026-01-15 16:24:18 -0800

    Lesson 4.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,8 @@
 export default function (eleventyConfig) {
 	eleventyConfig.setInputDirectory('src');
 	eleventyConfig.setOutputDirectory('dist');
+	// Set directories to pass through to the dist folder
+	eleventyConfig.addPassthroughCopy('src/images');
 }
 export const config = {
 	markdownTemplateEngine: 'njk',
--- src/index.md
+++ src/index.md
@@ -1,5 +1,13 @@
 ---
 title: 'Hello, world'
 layout: 'layouts/home.html'
+intro:
+  eyebrow: 'Cats, DevOps and Art'
+  main: 'Polite'
+  summary: 'Look at the kitty.'
+  buttonText: ' View Kitty'
+  buttonUrl: '/work'
+  image: '/images/polite_cat.png'
+  imageAlt: 'Polite Cat'
 ---
 This is pretty _rad_, right?
