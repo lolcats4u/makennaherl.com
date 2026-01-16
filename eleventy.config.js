24fbe40079f45cbc90031c1138abbcf115ea937e
Makenna Herl <hire.makenna.herl@gmail.com>
2026-01-15 18:08:34 -0800

    Lesson 9 and lesson 10.

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,12 +5,27 @@ export default function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/images');
 	// Returns work items, sorted by display order
 	eleventyConfig.addCollection('work', (collection) => {
-		return collection
-			.getFilteredByGlob('./src/work/*.md')
-			.sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
+		return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+	});
+	// Creates and returns a collection of work that is set to be featured
+	eleventyConfig.addCollection('featuredWork', (collection) => {
+		return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+			(x) => x.data.featured,
+		);
 	});
 }
 export const config = {
 	markdownTemplateEngine: 'njk',
 	htmlTemplateEngine: 'njk',
 };
+/**
+ * Takes a collection and returns it back in display order
+ *
+ * @param {Array} collection The 11ty collection
+ * @returns {Array} the sorted collection
+ */
+function sortByDisplayOrder(collection) {
+	return collection.sort((a, b) =>
+		Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1,
+	);
+}
--- src/index.md
+++ src/index.md
@@ -1,5 +1,5 @@
 ---
-title: 'Hello, world'
+title: 'Makennas Site'
 layout: 'layouts/home.html'
 intro:
   eyebrow: 'Cats, DevOps and Art'
@@ -14,5 +14,10 @@ primaryCTA:
   summary: 'I built this with Learn Eleventy'
   buttonText: 'Buy me a coffee'
   buttonUrl: 'go_to_coffee'
+featuredWork:
+  title: 'Selected work'
+  summary: 'stuff I spent more than 5 minutes thinking about'
+studioFeed:
+  title: 'From inside the studio'
 ---
 This is pretty _rad_, right?
